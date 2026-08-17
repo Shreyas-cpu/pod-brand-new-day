f29cf73df6a52039fa884145c2f38e9c2bed44cf
Shreyas-cpu <shreyasmudholkar12345@gmail.com>
2026-08-17 19:11:54 +0530

    Remove curl_close from check.php to prevent JSON corruption

--- api/check.php
+++ api/check.php
@@ -151,7 +151,6 @@ foreach ($models as $model) {
     $response = curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     $curlError = curl_error($ch);
-    curl_close($ch);
 
     if ($response !== false && $httpCode >= 200 && $httpCode < 400) {
         $usedModel = $model;
